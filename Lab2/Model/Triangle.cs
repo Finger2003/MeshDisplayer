e85de1088c324bf09f5c8b2eb950da74885b3688
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2024-11-01 12:31:46 +0100

    Create Model

---
+++ Lab2/Model/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Model
+{
+    public class Triangle
+    {
+        public Vertex V1 { get; set; }
+        public Vertex V2 { get; set; }
+        public Vertex V3 { get; set; }
+    }
+}
---
+++ Lab2/Model/Vertex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Model
+{
+    public class Vertex
+    {
+        public struct State
+        {
+            public Vector3 P;
+            public Vector3 Pu;
+            public Vector3 Pv;
+            public Vector3 N;
+        }
+
+        public State BeforeRotationState { get; set; }
+        public State AfterRotationState { get; set; }
+    }
+}
